f957e37d37f9633489dbe7f23d1858de949f1053
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-11-15 16:24:09 -0300

    feat: Fetching data from the API using SERVER COMPONENTS.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,32 @@
-export default function Posts() {
+// SERVER COMPONENTS
+
+type PostProps = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
+type resProps = {
+  posts: PostProps[];
+};
+
+export default async function Posts() {
+  const res = await fetch("https://dummyjson.com/posts");
+  const data: resProps = await res.json();
+
   return (
-    <div className="flex h-screen pt-20">
-      <h1>Posts</h1>
+    <div className="flex flex-col h-screen pt-20">
+      <h1 className="text-center mt-5 mb-2">Posts</h1>
+
+      <div className="flex flex-col gap-4 mx-4">
+        {data.posts.map((post) => (
+          <div key={post.id} className="bg-gray-600 p-4 rounded-md">
+            <h2 className="font-bold">{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
